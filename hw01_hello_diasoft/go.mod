46f2c0c53db2b5696c064d7386cec59f67efa1de
Динар Фатхрахманов <dfatkhrakhmanov@diasoft.ru>
2025-06-25 10:41:05 +0300

    изменил настройки

--- hw01_hello_diasoft/go.mod
+++ hw01_hello_diasoft/go.mod
@@ -1,6 +1,7 @@
 module github.com/Dinfat696/hw01_hello_diasoft
 
-go 1.18
+go 1.23.0
 
+toolchain go1.24.4
 
-
+require golang.org/x/example/hello v0.0.0-20250605160450-8b405629c4a5
--- hw01_hello_diasoft/main.go
+++ hw01_hello_diasoft/main.go
@@ -1,15 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func reverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
+	//nolint:depguard
+	"golang.org/x/example/hello/reverse"
+)
 
 func main() {
-	fmt.Println(reverse("Hello, DIASOFT!"))
+	fmt.Println(reverse.String("Hello, DIASOFT!"))
 }

000b0ce47cc2670e7c27a3ed81fcffccd899dd99
Динар Фатхрахманов <dfatkhrakhmanov@diasoft.ru>
2025-06-30 15:36:28 +0300

    test

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -18,6 +18,7 @@ func TestUnpack(t *testing.T) {
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
 		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `aaas\3\3`, expected: `aaas33`},
 		// {input: `qwe\45`, expected: `qwe44444`},
 		// {input: `qwe\\5`, expected: `qwe\\\\\`},
 		// {input: `qwe\\\3`, expected: `qwe\3`},

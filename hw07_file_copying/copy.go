aeccff4182fa8c325d0698e90a01017bb1556ff1
Динар Фатхрахманов <dfatkhrakhmanov@diasoft.ru>
2026-02-03 19:30:30 +0300

    5

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -13,49 +13,71 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	var buf []byte
-	if limit > 0 {
-		buf = make([]byte, limit)
-	} else {
-		buf = make([]byte, 1024)
+	src, err := os.Open(fromPath)
+	if err != nil {
+		return err
 	}
-	fileFrom, openingError := os.Open(fromPath)
-	if openingError != nil {
-		return openingError
-	}
-	stat, err := os.Stat(fromPath)
+	defer src.Close()
+
+	stat, err := src.Stat()
 	if err != nil {
 		return ErrUnsupportedFile
 	}
+
 	size := stat.Size()
-	if size < offset {
+	if offset > size {
 		return ErrOffsetExceedsFileSize
 	}
-	copiedSize := size - offset
-	if limit < copiedSize && limit != 0 {
-		copiedSize = limit
-	}
-	fileTo, _ := os.Create(toPath)
-	currentSize := 0
-	var position int64
-	for position < copiedSize {
-		n, readingError := fileFrom.ReadAt(buf, offset+position)
-		position += int64(n)
-		if readingError != nil && !errors.Is(readingError, io.EOF) {
-			return readingError
-		}
-		write, writingError := fileTo.Write(buf[:n])
-		alreadyCopied := float64(currentSize + write)
-		fmt.Printf("Current percentage %.2f%%'\n'", alreadyCopied/(float64(copiedSize))*100)
-		currentSize += write
-		if writingError != nil {
-			return writingError
-		}
-	}
-	defer func() {
-		fileFrom.Close()
-		fileTo.Close()
-	}()
 
+	if offset > 0 {
+		_, err = src.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
+	bytesToCopy := size - offset
+	if limit > 0 && limit < bytesToCopy {
+		bytesToCopy = limit
+	}
+
+	dst, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	const bufSize = 4096
+	buf := make([]byte, bufSize)
+	var copied int64
+
+	for copied < bytesToCopy {
+		remaining := bytesToCopy - copied
+		readSize := bufSize
+		if remaining < int64(bufSize) {
+			readSize = int(remaining)
+		}
+
+		n, err := src.Read(buf[:readSize])
+		if n > 0 {
+			_, writeErr := dst.Write(buf[:n])
+			if writeErr != nil {
+				return writeErr
+			}
+			copied += int64(n)
+
+			percent := float64(copied) / float64(bytesToCopy) * 100
+			fmt.Printf("\rProgress: %.2f%%", percent)
+		}
+
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Println()
 	return nil
 }

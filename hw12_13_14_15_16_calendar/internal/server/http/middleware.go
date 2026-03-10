c05bb76bbf4c2bcab481ecf2425dea32ece3bde1
Динар Фатхрахманов <dfatkhrakhmanov@diasoft.ru>
2026-03-10 14:28:24 +0300

    20

--- hw12_13_14_15_16_calendar/internal/server/http/middleware.go
+++ hw12_13_14_15_16_calendar/internal/server/http/middleware.go
@@ -5,9 +5,4 @@ import (
 	"net/http"
 )
 
-func loggingMiddleware(next http.Handler) http.Handler { //nolint:unused
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r.Body)
-		next.ServeHTTP(w, r)
-	})
-}
+

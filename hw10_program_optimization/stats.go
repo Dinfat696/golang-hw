9030b81a9f0a960804ac3cb0e59d6cb79491e5bc
Динар Фатхрахманов <dfatkhrakhmanov@diasoft.ru>
2026-03-17 15:16:12 +0300

    4

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -2,6 +2,7 @@ package hw10programoptimization
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
@@ -27,7 +28,7 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	for {
 		var u User
 		err := dec.Decode(&u)
-		if err == io.EOF {
+		if errors.Is(err, io.EOF) { // исправлено
 			break
 		}
 		if err != nil {
